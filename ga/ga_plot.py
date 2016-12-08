767d8f7336f5b200eef037d4b670d6670f59b172
Irina <irinabarykina@gmail.com>
2016-12-08 18:28:38 +0100

    Second world (35)

--- ga/ga_plot.py
+++ ga/ga_plot.py
@@ -1,11 +1,13 @@
 from ga import City, World, GA
 
 import csv
+import numpy as np
 import matplotlib.pyplot as plt
 import random
 import copy
 import os
 import jsonpickle
+from matplotlib.legend_handler import HandlerLine2D
 from multiprocessing.dummy import Pool as ThreadPool
 
 
@@ -85,26 +87,26 @@ def save_xps(filename, data):
 
 
 class XP(object):
-    def __init__(self, migration_interval, num_migrants, world, fitness_thresholds):
+    def __init__(self, migration_interval, num_migrants, world):
         self.migration_interval = migration_interval
         self.num_migrants = num_migrants
         self.world = world
-        self.fitness_thresholds = fitness_thresholds
+        #self.fitness_thresholds = fitness_thresholds
 
-    def format_data(self, xp_num, generation_num, fitness, distance, num_migrants, migr_int, threshold):
-        return '{},{},{},{},{},{},{}\n'.format(xp_num, generation_num, fitness, distance, num_migrants, migr_int, threshold)
+    def format_data(self, xp_num, generation_num, fitness, distance, num_migrants, migr_int):
+        return '{},{},{},{},{},{}\n'.format(xp_num, generation_num, fitness, distance, num_migrants, migr_int)
 
     def run_xp(self, xp_num):
         results = []
         # init islands
         islands = []
-        for i in range(1, 5):
-            island = GA(self.world, 0.015, 5, True, 350, 'Island:' + str(i))
+        for i in range(0, 5):
+            island = GA(self.world, 0.015, 5, True, 80, 'Island:' + str(i))
             islands.append(island)
 
         # run evolution
-        thresholds = copy.copy(self.fitness_thresholds)
-        for gen in range(1, 500):
+        #thresholds = copy.copy(self.fitness_thresholds)
+        for gen in range(1, 300):
             if gen % self.migration_interval == 0:
                 migrate(islands, self.num_migrants)
 
@@ -112,14 +114,14 @@ class XP(object):
                 island.run(1)
 
             fittest = find_fittest_island(islands)
-            threshold = thresholds[0]
+            #threshold = thresholds[0]
 
-            if fittest.get_distance() < threshold:
-                results.append(self.format_data(xp_num, gen, fittest.get_fitness(), fittest.get_distance(),
-                                        self.num_migrants, self.migration_interval, threshold))
-                thresholds.pop(0)
-                if len(thresholds) == 0:
-                    return results
+            #if fittest.get_distance() < threshold:
+            results.append(self.format_data(xp_num, gen, fittest.get_fitness(), fittest.get_distance(),
+                                        self.num_migrants, self.migration_interval))
+                #thresholds.pop(0)
+                #if len(thresholds) == 0:
+                    #return results
 
         return results
 
@@ -134,19 +136,18 @@ def explore_migration_interval():
         save_world(world, 'world.json')
 
     max_xp_num = 10
-    num_migrants = 5
-    fitness_thresholds = [11000, 6500]
+    num_migrants = 2
 
-    for migration_interval in range(1, 250, 2):
+    for migration_interval in range(41, 52, 10):
         print 'Run ' + str(max_xp_num) + ' XPs for migration interval ' + str(migration_interval)
-        xp = XP(migration_interval, num_migrants, world, fitness_thresholds)
+        xp = XP(migration_interval, num_migrants, world)
 
-        pool = ThreadPool(8)
+        pool = ThreadPool(2)
         results = pool.map(xp.run_xp, range(0, max_xp_num))
         pool.close()
         pool.join()
 
-        save_xps('experiment_data1.csv', results)
+        save_xps('experiment_data_41_51.csv', results)
         print 'Finish XPs for migration interval ' + str(migration_interval)
 
 
@@ -158,28 +159,30 @@ def animate_island_model():
         world = generate_world()
         save_world(world, 'world.json')
 
-    migration_interval = 10
+    migration_interval = 60
     num_migrants = 1
     print 'Migration pattern: ' + str(migration_interval) + str(num_migrants)
 
     plt.ion()
     # init islands
     islands = []
-    for i in range(1, 5):
-        island = GA(world, 0.015, 5, True, 400, 'Island:' + str(i))
+    for i in range(0, 5):
+        island = GA(world, 0.015, 5, True, 90, 'Island:' + str(i))
         islands.append(island)
 
     # prepare visualization
     fig = plt.figure()
     fig.subplots_adjust(left=0.2, wspace=0.6)
-    ax1 = fig.add_subplot(221)
+    ax1 = fig.add_subplot(231)
     plot_route(ax1, islands[0].get_best())
-    ax2 = fig.add_subplot(222)
+    ax2 = fig.add_subplot(232)
     plot_route(ax2, islands[1].get_best())
-    ax3 = fig.add_subplot(223)
+    ax3 = fig.add_subplot(233)
     plot_route(ax3, islands[2].get_best())
-    ax4 = fig.add_subplot(224)
+    ax4 = fig.add_subplot(234)
     plot_route(ax4, islands[3].get_best())
+    ax5 = fig.add_subplot(235)
+    plot_route(ax5, islands[4].get_best())
     plt.pause(0.24)
 
     # run evolution
@@ -187,27 +190,69 @@ def animate_island_model():
         # evaluate N populations on every island
         for island in islands:
             island.run(migration_interval)
-        # migrate(islands, num_migrants)
+            migrate(islands, num_migrants)
         # plot fittest for each island
 
         plot_route(ax1, islands[0].get_best())
         plot_route(ax2, islands[1].get_best())
         plot_route(ax3, islands[2].get_best())
         plot_route(ax4, islands[3].get_best())
+        plot_route(ax5, islands[4].get_best())
 
         fittest = find_fittest_island(islands)
         print str(fittest.get_distance())
 
         plt.pause(1)
 
-def load_experimental_data():
-    with open('experiment_data.csv', 'rb') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in spamreader:
-            print ', '.join(row)
+
+def load_and_plot_experimental_data():
+    data = []
+    with open('experiment_data2.csv', 'rb') as csvfile:
+        row_data = csv.reader(csvfile, delimiter=',', quotechar='|')
+        curr_int= 1
+
+        gen_to_fit = []
+        plot_data = []
+
+        for row in row_data:
+            xp_num = int(row[0])
+            gen_num = int(row[1])
+            interval = int(row[5])
+            fitness = float(row[2])
+
+            if interval != curr_int:
+                for gen in gen_to_fit:
+                    plot_data.append(np.mean(gen))
+                data.append((curr_int, plot_data))
+                curr_int = interval
+
+                gen_to_fit = []
+                plot_data = []
+
+            print str(gen_num) + ' ' + str (len(gen_to_fit)) + ' ' + str(interval)
+            if len(gen_to_fit) < gen_num:
+                gen_to_fit.append([])
+
+            gen_to_fit[gen_num-1].append(fitness)
+
+        for gen in gen_to_fit:
+            plot_data.append(np.mean(gen))
+        data.append((curr_int, plot_data))
+
+    line1, = plt.plot(range(0, len(data[0][1])), data[0][1], 'm-', label='interval=' + str(data[0][0]))
+    line2, = plt.plot(range(0, len(data[1][1])), data[1][1], 'r-', label='interval=' + str(data[1][0]))
+    line3, = plt.plot(range(0, len(data[2][1])), data[2][1], 'b-', label='interval=' + str(data[2][0]))
+    line4, = plt.plot(range(0, len(data[3][1])), data[3][1], 'g-', label='interval=' + str(data[3][0]))
+
+    plt.legend(handles=[line1, line2, line3, line4])
+    plt.ylabel('fitness')
+    plt.xlabel('generation')
+
+    plt.show()
+
 
 
 if __name__ == "__main__":
     #animate_island_model()
-    explore_migration_interval()
-    #load_and_plot_experimental_data()
+    #explore_migration_interval()
+    load_and_plot_experimental_data()
